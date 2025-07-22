d13e3dfe64d87c145448018c1d7172b084af47cf
Alex <sanasutik@gmail.com>
2025-07-22 17:42:44 +0300

    add hotkey change

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 
-from assets.generatedUI.main_widget import Ui_Form
+from assets.generatedUI.main_widget import Ui_MainWidget
+from assets.generatedUI.hotkey_widget import Ui_HotkeyWidget
 import pydirectinput
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtCore import QThread, pyqtSignal, QFile, QTextStream
@@ -13,6 +14,43 @@ def load_stylesheet(filename: str) -> str:
     with open(filename, 'r', encoding='utf-8') as f:
         return '\n'.join(f.readlines())
 
+
+class HotkeyRecorder(QThread):
+    hotkey_recorded = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.keys_pressed = []
+
+    def run(self):
+        def on_press(key):
+            try:
+                key_str = key.char
+                if '_l' in key_str or '_r' in key_str:
+                    key_str = key_str[:-2]
+            except AttributeError:
+                key_str = str(key).replace("Key.", "")
+                if '_l' in key_str or '_r' in key_str:
+                    key_str = key_str[:-2]
+
+            if key_str not in self.keys_pressed:
+                self.keys_pressed.append(key_str)
+            return True
+
+        def on_release(key):
+            try:
+                key_str = key.char
+            except AttributeError:
+                key_str = str(key).replace("Key.", "")
+
+            if key_str in self.keys_pressed:
+                self.hotkey_recorded.emit('+'.join(self.keys_pressed))
+                print('+'.join(self.keys_pressed))
+                self.keys_pressed.clear()
+                return False
+
+        with pynput.keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
 
 class KeyListenerThread(QThread):
     key_pressed = pyqtSignal(str)
@@ -91,15 +129,19 @@ class ClickerThread(QThread):
         self.running = False
 
 
-class Widget(QWidget, Ui_Form):
+class MainWidget(QWidget, Ui_MainWidget):
     def __init__(self):
         super().__init__()
+        self.hotkey = 'F6'
+        self.hotkeyForm = HotkeyWidget(self)
+        self.hotkeyForm.hide()
         self.initUI()
         self.hotkeys()
         self.auto = False
 
     def hotkeys(self):
-        keyboard.add_hotkey('f6', self.toggle_clicker)
+        keyboard.add_hotkey(self.hotkey, self.toggle_clicker)
+
 
     def initUI(self):
         self.setupUi(self)
@@ -119,6 +161,8 @@ class Widget(QWidget, Ui_Form):
         self.startButton.clicked.connect(self.toggle_clicker)
         self.stopButton.clicked.connect(self.toggle_clicker)
         self.setKeyButton.clicked.connect(self.startKeyCapture)
+        self.hotkeyButton.clicked.connect(self.openForm)
+        self.hotkeyForm.data_sent.connect(self.hotkeyChange)
 
     def toggle_clicker(self):
         if self.auto:
@@ -175,10 +219,51 @@ class Widget(QWidget, Ui_Form):
         self.setKeyButton.setEnabled(True)
         self.keyLabel.setText(key.upper())
 
+    def openForm(self):
+        if self.hotkeyForm.isVisible():
+            if self.hotkeyForm.isMinimized():
+                self.hotkeyForm.showNormal()
+            self.hotkeyForm.raise_()
+            self.hotkeyForm.activateWindow()
+        else:
+            self.hotkeyForm.show()
+
+    def hotkeyChange(self, keys):
+        self.hotkey = keys
+        self.hotkeys()
+        self.hotkeyForm.hotkeyLabel.setText(keys)
+
+    def closeEvent(self, a0):
+        if self.hotkeyForm.isVisible():
+            self.hotkeyForm.close()
+
+class HotkeyWidget(QWidget, Ui_HotkeyWidget):
+    data_sent = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setupUi(self)
+        self.setStyleSheet(load_stylesheet('assets/styles/night.css'))
+        self.setFixedSize(300, 100)
+        self.parent = parent
+        self.setHotkeyButton.clicked.connect(self.startKeyCapture)
+
+    def startKeyCapture(self):
+        self.setHotkeyButton.setEnabled(False)
+
+        self.hotkey_thread = HotkeyRecorder()
+        self.hotkey_thread.hotkey_recorded.connect(self.updateKey)
+        self.hotkey_thread.start()
+
+    def updateKey(self, key):
+        self.data_sent.emit(key)
+        self.setHotkeyButton.setEnabled(True)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
-    window = Widget()
+    window = MainWidget()
     window.show()
     app.exec()
